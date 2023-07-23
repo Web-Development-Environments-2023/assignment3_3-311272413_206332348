f286392a9725cbe92d592082e7edaa63db1ffc2c
KathyAgafonov <kathyagafonov@gmail.com>
2023-07-23 21:50:22 +0300

    update LoginPage

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,37 +1,62 @@
 <template>
   <div class="container">
     <h1 class="title">Login</h1>
-
     <b-form @submit.prevent="onLogin">
-      <!-- Username -->
-      <b-form-group id="input-group-Username" label-cols-sm="3" label="Username:" label-for="Username">
-        <b-form-input id="Username" v-model="$v.form.username.$model" type="text"
-          :state="validateState('username')"></b-form-input>
+      <b-form-group
+        id="input-group-Username"
+        label-cols-sm="3"
+        label="Username:"
+        label-for="Username"
+      >
+        <b-form-input
+          id="Username"
+          v-model="$v.form.username.$model"
+          type="text"
+          :state="validateState('username')"
+        ></b-form-input>
         <b-form-invalid-feedback>
           Username is required
         </b-form-invalid-feedback>
       </b-form-group>
 
-      <!-- Password -->
-      <b-form-group id="input-group-Password" label-cols-sm="3" label="Password:" label-for="Password">
-        <b-form-input id="Password" type="password" v-model="$v.form.password.$model"
-          :state="validateState('password')"></b-form-input>
+      <b-form-group
+        id="input-group-Password"
+        label-cols-sm="3"
+        label="Password:"
+        label-for="Password"
+      >
+        <b-form-input
+          id="Password"
+          type="password"
+          v-model="$v.form.password.$model"
+          :state="validateState('password')"
+        ></b-form-input>
         <b-form-invalid-feedback>
           Password is required
         </b-form-invalid-feedback>
       </b-form-group>
 
-      <b-button type="submit" variant="primary" style="width:100px;display:block;" class="mx-auto w-100">Login</b-button>
+      <b-button
+        type="submit"
+        variant="primary"
+        style="width:100px;display:block;"
+        class="mx-auto w-100"
+        >Login</b-button
+      >
       <div class="mt-2">
         Do not have an account yet?
         <router-link to="register"> Register in here</router-link>
       </div>
     </b-form>
-
-    <b-alert class="mt-2" v-if="form.submitError" variant="warning" dismissible show>
+    <b-alert
+      class="mt-2"
+      v-if="form.submitError"
+      variant="warning"
+      dismissible
+      show
+    >
       Login failed: {{ form.submitError }}
     </b-alert>
-
   </div>
 </template>
 
@@ -44,19 +69,19 @@ export default {
       form: {
         username: "",
         password: "",
-        submitError: undefined
-      }
+        submitError: undefined,
+      },
     };
   },
   validations: {
     form: {
       username: {
-        required
+        required,
       },
       password: {
-        required
-      }
-    }
+        required,
+      },
+    },
   },
   methods: {
     validateState(param) {
@@ -69,12 +94,9 @@ export default {
           this.$root.store.server_domain + "/Login",
           {
             username: this.form.username,
-            password: this.form.password
+            password: this.form.password,
           }
         );
-        // console.log(response);
-        // this.$root.loggedIn = true;
-        console.log(this.$root.store.login);
         this.$root.store.login(this.form.username);
         this.$router.push("/");
       } catch (err) {
@@ -89,8 +111,8 @@ export default {
         return;
       }
       this.Login();
-    }
-  }
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
